7c3cc39813902835e044d2101c1d7d6e86baea18
TamableCoder064 <160609772+TamableCoder064@users.noreply.github.com>
2024-05-14 21:02:35 +0200

    Add files via upload

--- Apuntes/Apuntes_T7.py
+++ Apuntes/Apuntes_T7.py
@@ -1,26 +1,32 @@
 # TEMA 4
 # Paradigma Orientado a Objetos
 # Es un diseño de software que nos permite representar objetos de la vida real
-# Dos partes: los datos y la forma en la que nos relacionamos
-# Los objetos tienen un núcleo que son los datos que almacenamos en ese objeto
-# La capa exterior son las funciones (métodos) de ese objeto
 # Sirve para separar componentes de un programa
+
+# Dos partes: los datos (Parte NO visible) y la forma en la que nos relacionamos (Parte visible)
+#   Los objetos tienen un núcleo que son los datos que almacenamos en ese objeto
+#   La capa exterior son las funciones (métodos) de ese objeto
+
 # Atributos es la parte de información que definen al objeto
 # Los eventos es cuando llamo a una función de un objeto
 # La clase es la plantilla, a raiz de esa plantilla creas objetos
 # Un objeto es la instancia de una clase
 # Instanciar es definir un objeto
+
 # Abstraccion: Dependiendo del contexto de tu clase vas a enseñar más o menos información de tu clase
+
 # Encapsulación: Cierra los atributos del nucleo lo suficiente para que se interactue con el nucleo a traves de los métodos (clases)
-# REGLA ENCAPSULACIÓN: Una clase no puede depender de un factor externo
+#                Una clase no puede depender de un factor externo
+
 # Polimorfismo: permite a distintos objetos responder de manera diferente a un mismo llamado de método
 #   Sobrecarga (overload): Dos metodos con el mismo nombre pero distintos parametros
-#   Sobreescritura (overriding): En la herencia se puede sobreescribir
-# Herencia: Nos sirve para generar una gerarquía de clases sin repetir atributos comunes
+#   Sobreescritura (overriding): En la herencia se puede sobreescribir en la clase hija
+# Python no implementa Sobrecarga
+
+# Herencia: Nos sirve para generar una gerarquía de clases sin repetir atributos y métodos comunes
 # Modularidad: Segmentar el código y reutilizarlo tantas veces como necesitamos
 # Recolección de basura: Es importante que se vayan quitando los objetos que no se usan
 # Refactorizar: Que no haya trozos de código duplicados
-# Todo en python es una clase (incluídos los str, bool, int...)
 
 # Cómo hacemos una clase:
 class NombreClase:
@@ -28,11 +34,11 @@ class NombreClase:
 
 # La clase define tipos de objetos (ej, la clase mesa crea objetos mesa)
 
-# Buena práctica, los atributos de la clase se debería definir en el constructor de la clase
+# Buena práctica, los atributos de la clase se debería definir (inicializar) en el constructor de la clase
 
 # Cómo instanciamos un objeto:
-obj = NombreClase() # Con esto creamos un objeto que pertenece a la clase NombreClase con la propiedad 1
-print(type(obj))
+obj = NombreClase() # Con esto creamos un objeto que pertenece a la clase NombreClase con la propiedad 1 como parte de su núcleo
+print(type(obj))    # El tipo de un objeto es el nombre de la clase
 
 # Ejemplo:
 class Saludo: # Define la clase
@@ -46,13 +52,41 @@ p1.saludar("Oscar") # Utiliza la función saludar en la variable p1
 
 # Existe un método reservado llamado __init__ que es el constructor de la clase
 # Será el método al que se llame cuando se instancie un objeto de esa clase
+# Se utiliza para 'Iniciar' los atributos del objeto
+# El destructor se omite en python con el Garbage Colector
 
 class Saludo:
-    def __init__(self, texto):
-        self.texto = texto
+    def __init__(self, texto): # Definir un parámetro de entrada en el constructor de una clase lo hace "obligatorio"
+        self.texto = texto # Le asignamos el valor de la variable texto al atributo self.texto
     def saludar(self, name):
         print(self.texto + " " + name + "!!!!")
 
 p1 = Saludo("Hola")
 p1.saludar("Clase")
 
+# Constructor vacío
+class Vacío():
+    def __init__(self):
+        pass
+# Pongas o no pongas el constructor vacío python ya lo hace solo
+# Si no se va a aportar nada más se considera redundante
+
+# Definir un método:
+class NombreClase:
+    def miFuncion(self):
+        print('Hola Mundo!!!')
+# self solo se usa dentro de la clase
+
+# Acceder a un atributo:
+class Saludo:
+    
+    def __init__(self, nombre):
+        self.texto = 'Hola'
+        self.nombre = nombre
+    
+    def saludar(self):
+        print('Método -> ' + self.texto + ' ' + self.nombre)
+
+p1 = Saludo('clase')
+p1.saludar()
+print('Atributos -> ' + p1.texto + ' ' + p1.nombre)
